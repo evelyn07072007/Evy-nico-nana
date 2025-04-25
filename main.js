d6e62a339ec9686be16fc87c40e4968ec86630c5
evelyn07072007 <160125244+evelyn07072007@users.noreply.github.com>
2025-04-25 10:55:37 -0300

    anna,evelyn e nicolly

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ Math.floor(Math.random() *10 ) + 1;
 let attempts = 0;
 
  function checkGuess () {
-      const userGuess = parseInt(document.getElementById( 'guess').ariaValueNow)
+      const userGuess = parseInt(document.getElementById( 'guess').ariaValueMax
  }
